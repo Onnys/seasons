75c016bca931c59a855fc4bd99e066b167f4d3f6
onnys <onnysanild@gmail.com>
2022-03-12 00:13:47 +0200

    solved a bug when displaying latitude

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null, errorMessage: "" };
+    this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -21,9 +21,9 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      <div>Latitude: {this.state.lat}</div>;
+      return <div>Latitude: {this.state.lat}</div>;
     }
-    
+
     return <div>Loading!</div>;
   }
 }
