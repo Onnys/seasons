bad9c4086a74df01f83ee6342a7eac887288916d
onnys <onnysanild@gmail.com>
2022-03-12 08:21:50 +0200

    refactored code from seasonDisplay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const seasonInfo = {
+  summer: {
+    text: "Let's have a beer",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Let's have a hot chocolate",
+    iconName: "snowflake",
+  },
+};
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -8,9 +18,18 @@ const getSeason = (lat, month) => {
     return lat > 0 ? "winter" : "summer";
   }
 };
+
 const SeasonDisplay = (props) => {
   const season = getSeason(props.latitude, new Date().getMonth());
-  return <div>{season}</div>;
+
+  const { text, iconName } = seasonInfo[season];
+  return (
+    <div>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
+    </div>
+  );
 };
 
 ReactDOM.render(<SeasonDisplay />, document.querySelector("#root"));
